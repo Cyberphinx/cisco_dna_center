fa0492a6f5366e141a1b4be67a15447e505391e5
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2024-07-22 14:43:35 +0100

    feat: Authentication

--- src/main.py
+++ src/main.py
@@ -7,3 +7,24 @@ requests.packages.urllib3.disable_warnings()
 
 def get_auth_token():
     # Endpoint URL
+    endpoint = '/dna/system/api/v1/auth/token'
+    url = 'https://' + env_lab.DNA_CENTER['host'] + endpoint
+    # Make the POST request
+    response = requests.post(
+        url,
+        auth=HTTPBasicAuth(
+            env_lab.DNA_CENTER['username'],
+            env_lab.DNA_CENTER['password']
+        ),
+        verify=False
+    )
+    # Retrieve the Token from the returned JSON
+    token = response.json()['Token']
+    # Print out the Token
+    print("Token Retrieved: {}".format(token))
+    # Create a return statement to send the token back for later use
+    return token
+
+
+if __name__ == "__main__":
+    get_auth_token()
